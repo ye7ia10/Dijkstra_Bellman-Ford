5d0294ce81fff0d353b6d8d2511c000a64ee81bb
ye7ia10 <yehiaelsayed234@gmail.com>
2019-05-17 00:43:23 +0200

    implement interface + Edge class

---
+++ src/eg/edu/alexu/csd/filestructure/graphs/test/cs59_07/Edge.java
@@ -0,0 +1,36 @@
+package eg.edu.alexu.csd.filestructure.graphs.test.cs59_07;
+
+public class Edge {
+	
+	private int from;
+	private int to;
+	private int weight;
+	
+	public Edge() {}
+	
+	public Edge(int from, int to, int weight) {
+		this.from = from;
+		this.to = to;
+		this.weight = weight;
+	}
+	
+	public int getFrom() {
+		return from;
+	}
+	public void setFrom(int from) {
+		this.from = from;
+	}
+	public int getTo() {
+		return to;
+	}
+	public void setTo(int to) {
+		this.to = to;
+	}
+	public int getWeight() {
+		return weight;
+	}
+	public void setWeight(int weight) {
+		this.weight = weight;
+	}
+
+}
---
+++ src/eg/edu/alexu/csd/filestructure/graphs/test/cs59_07/Graph.java
@@ -0,0 +1,52 @@
+package eg.edu.alexu.csd.filestructure.graphs.test.cs59_07;
+
+import java.io.File;
+import java.util.ArrayList;
+
+import eg.edu.alexu.csd.filestructure.graphs.IGraph;
+
+public class Graph implements IGraph{
+
+	@Override
+	public void readGraph(File file) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int size() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public ArrayList<Integer> getVertices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Integer> getNeighbors(int v) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void runDijkstra(int src, int[] distances) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Integer> getDijkstraProcessedOrder() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean runBellmanFord(int src, int[] distances) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
